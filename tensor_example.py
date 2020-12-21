ccf4195db6cbd52a5de28eabecd81002327b9854
dinothia <dinossht@stud.ntnu.no>
2020-12-21 12:30:46 +0100

    tensor example complete

--- tensor_example.py
+++ tensor_example.py
@@ -22,5 +22,22 @@ for t in range(2000):
     y_pred = a + b * x + c * x ** 2 + d * x ** 3
 
     # Compute and print loss
+    loss = (y_pred - y).pow(2).sum().item()
     if t % 100 == 99:
         print(t, loss)
+
+    # Backprop to compute gradients of a, b, c, d with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_a = grad_y_pred.sum()
+    grad_b = (grad_y_pred * x).sum()
+    grad_c = (grad_y_pred * x ** 2).sum()
+    grad_d = (grad_y_pred * x ** 3).sum()
+
+    # Update weights using gradient descent
+    a -= learning_rate * grad_a
+    b -= learning_rate * grad_b
+    c -= learning_rate * grad_c
+    d -= learning_rate * grad_d
+
+print(f"Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3")
+
