0ea304b64370b822576c192edbaeb49501d89fe3
dinothia <dinossht@stud.ntnu.no>
2020-12-21 12:14:13 +0100

    tensor example

---
+++ tensor_example.py
@@ -0,0 +1,26 @@
+import torch
+import math
+
+
+dtype = torch.float
+device = torch.device("cpu")
+# device = torch.device("cudo:0") # uncomment to run this on GPU
+
+# Create random input and output
+x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
+y = torch.sin(x)
+
+# Randomly initialize weights
+a = torch.randn((), device=device, dtype=dtype)
+b = torch.randn((), device=device, dtype=dtype)
+c = torch.randn((), device=device, dtype=dtype)
+d = torch.randn((), device=device, dtype=dtype)
+
+learning_rate = 1e-6
+for t in range(2000):
+    # Forward pass: compute predicted y
+    y_pred = a + b * x + c * x ** 2 + d * x ** 3
+
+    # Compute and print loss
+    if t % 100 == 99:
+        print(t, loss)
