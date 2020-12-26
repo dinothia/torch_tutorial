c97ff2bd606f0e693c5e6d30933472f18fa79350
dinothia <dinossht@stud.ntnu.no>
2020-12-26 18:15:20 +0100

    working 20x20 inversion simple linear

--- linear_example.py
+++ linear_example.py
@@ -16,23 +16,23 @@ A = torch.tensor([
                 [4, 0], 
                 [0.5, 4]], dtype=torch.float)
 """
-A = torch.randn(3, 3)
+A = torch.randn(20, 20)
 
-D_I = A.shape[0]                
-D_O = A.shape[1]                
+D_I = A.shape[0]
+D_O = A.shape[1]
 
 b = 5
 x = torch.randn(N, D_I)
-#x = torch.randn(N, D_I)
+# x = torch.randn(N, D_I)
 y = x @ A + b
-#y += 0.1 * torch.randn_like(y)
+y += 0.01 * torch.randn_like(y)
 
 if swap_in_out:
     x_train = y
     y_train = x
 
-    D_I = A.shape[1]                
-    D_O = A.shape[0]                
+    D_I = A.shape[1]
+    D_O = A.shape[0]
 else:
     x_train = x
     y_train = y
@@ -42,19 +42,20 @@ bs = 16
 train_ds = TensorDataset(x_train, y_train)
 train_dl = DataLoader(train_ds, shuffle=True, batch_size=bs)
 
-lr = 1e-3
+#lr = 1e-3 #* (10**3 / 10**D_I)i
+lr = 1e-3 
 model = torch.nn.Sequential(
-        torch.nn.Linear(D_I, D_O), 
-        #torch.nn.ReLU()
+    torch.nn.Linear(D_I, D_O),
+    # torch.nn.ReLU()
 )
-        
-loss_fn = torch.nn.MSELoss(reduction="sum")
+
+loss_fn = torch.nn.MSELoss(reduction="mean")
 opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
 
 loss_arr = []
-epochs = 100
+epochs = 10000
 for epoch in range(epochs):
-    for xb,yb in train_dl:
+    for xb, yb in train_dl:
         pred = model(xb)
         loss = loss_fn(pred, yb)
 
@@ -67,12 +68,21 @@ for epoch in range(epochs):
         print(f"epoch: {epoch} loss: {loss.item()}")
 
 linear_layer = model[0]
-if swap_in_out: print("xy swapped:")
+
+if swap_in_out:
+    print("xy swapped:")
 print(f"bias term = \n{linear_layer.bias.detach().numpy()}")
 print(f"actual weight = \n {A.detach().numpy()}")
 print(f"inv actual weight = \n {np.linalg.inv(A.detach().numpy())}")
 print(f"weights term = \n{linear_layer.weight.detach().numpy().round(2).T}")
-#print(x_train[-1])
+# print(x_train[-1])
 
 plt.plot(loss_arr)
 plt.show()
+if swap_in_out:
+    plt.imshow(np.linalg.inv(A.detach().numpy()))
+    plt.show()
+plt.show()
+
+print(f"inv actual weight row 0 = \n {np.linalg.inv(A.detach().numpy())[0]}")
+print(f"weights term row 0 = \n{linear_layer.weight.detach().numpy().T[0]}")
