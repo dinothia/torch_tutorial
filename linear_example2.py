c28908c24c505504811ed3af436d3ff83ea41221
dinothia <dinossht@stud.ntnu.no>
2020-12-30 17:49:06 +0100

    adding constrained loss function

--- linear_example2.py
+++ linear_example2.py
@@ -3,28 +3,26 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-D1 = 20 
-D2 = 10
-N = 1000
-ITER = 1000
+D1 = 30
+D2 = 20
+N = 4000
+ITER = 500
 SWAP = False
 NOISE = True
 NORMALIZE = True
-VAL_PERC = 0.25
+VAL_PERC = 0.3
+learning_rate = 1e0
 
+
+W_gt = torch.randn(D1, D2)
+W_gt[:D2, :D2] += 10 * torch.eye(D2)
+b_gt = 0.01
 
 x = torch.randn(N, D1)
+y = x @ W_gt + b_gt 
 
-W_gt = torch.tensor([
-    [1.8, 0, 1.8], 
-    [0.1, 1, 0.1],
-    [1.8, 0, 1.8], 
-])
-W_gt = torch.randn(D1, D2)
-#b_gt = 4.0
-y = x @ W_gt# + b_gt
 if NOISE:
-    y += 0.5 * torch.randn(N, D2)
+    y += 0.1 * torch.randn(N, D2)
 
 if SWAP:
     x_copy = x
@@ -44,20 +42,32 @@ class LinearRegression(torch.nn.Module):
         super(LinearRegression, self).__init__() 
         if SWAP:
             self.linear = torch.nn.Linear(D2, D1, bias = True) # bias is default True
+            self.linear2 = torch.nn.Linear(D1, D1 * 1, bias = True) # bias is default True
+            self.linear3 = torch.nn.Linear(D1 * 1, D1, bias = True) # bias is default True
         else:
             self.linear = torch.nn.Linear(D1, D2, bias = True) # bias is default True
+            self.linear2 = torch.nn.Linear(D2, D2 * 1, bias = True) # bias is default True
+            self.linear3 = torch.nn.Linear(D2 * 1, D2, bias = True) # bias is default True
 
-        self.activation = torch.nn.Tanh()
+        self.activation1 = torch.nn.Tanh()
+        self.activation2 = torch.nn.Tanh()
+        self.activation3 = torch.nn.Tanh()
 
     def forward(self, x):
         x = self.linear(x)
-        x = self.activation(x)
+        x = self.activation1(x)
+        x = self.linear2(x)
+        #x = self.activation2(x)
+        #x = self.linear3(x)
+        #x = self.activation2(x)
         return x
 
     def my_loss(self, output, target):
         loss1 = torch.mean((output - target)**2)
         #loss2 = torch.mean((self.linear.weight[0][0] - W_gt[0][0])**2)
         loss2 = 0#torch.mean((self.linear.weight[0][0])**2)
+        loss2 = 0.1 * 1 / torch.trace(self.linear.weight)
+        #loss2 = torch.norm(self.linear.weight)
         loss = loss1 + loss2
         return loss
 
@@ -70,7 +80,7 @@ model = LinearRegression()
 
 criterion = torch.nn.MSELoss()
 criterion = model.my_loss
-optimizer = torch.optim.SGD(model.parameters(), lr = 1e-1)
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
 
 loss_arr = []
 val_arr = []
@@ -101,35 +111,29 @@ def trainBuildIn(model, x, y, iter):
         loss = criterion(y_pred, y_val)
         val_arr.append(loss.item())
 
-
-
-
-
-
 trainBuildIn(model, x_train, y_train, ITER)
 
-y_pred_bi = model(x_train).data.numpy()
+y_pred_bi = model(x_val).data.numpy()
 
 print("----- ----- ----- ----- -----")
 print("Prediction:")
-#for name, param in model.named_parameters():
-#    if param.requires_grad:
-#        print(name, param.data)
-#print("Ground-truth:")
-#print("w_gt = {:.2f}, b_gt = {:.2f}".format(w_gt ,b_gt))
+print(f"train loss: {round(loss_arr[-1], 4)}, val loss: {round(val_arr[-1], 4)}")
 
 plt.figure()
+plt.title("Validation")
 plt.clf()
 plt.plot(y[0], label='True data', alpha=0.5)
 plt.plot(y_pred_bi[0][0], label='Predictions', alpha=0.5)
 plt.legend(loc='best')
+
 plt.figure()
 plt.plot(loss_arr)
 plt.plot(val_arr)
+
 plt.figure()
 plt.subplot(211)
 if SWAP:
-    plt.title("SWAP-GT")
+    plt.title("PINV-GT")
     plt.imshow(np.linalg.pinv(W_gt).T)
 else:
     plt.title("GT")
@@ -139,3 +143,4 @@ plt.subplot(212)
 plt.title("Model")
 plt.imshow(model.linear.weight.detach())
 plt.show()
+
